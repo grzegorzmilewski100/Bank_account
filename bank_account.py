e19a28d0b944b3930d1d31a3eed0db8e3a616b04
Grzegorz Milewski <grzegorzmilewski@MacBook-Air-Grzegorz.local>
2022-08-15 16:23:58 +0200

    Creating user and bank class

--- bank_account.py
+++ bank_account.py
@@ -1,2 +1,32 @@
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def show_details(self):
+        print("Personal details")
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
 
+class Bank(User):
+    def __init__(self, name, age,):
+        super().__init__(name, age)
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Account balance has been updated: {self.balance}$")
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print(f"Your account has been updated: {self.balance}")
+        else:
+            print("You have not got enough money to withdraw")
+    def show_bank_account(self):
+        print(f"Your bank account is: {self.balance}$")
+
+johan = Bank("Johan", 25)
+johan.deposit(250)
+johan.withdraw(350)
+johan.show_bank_account()
